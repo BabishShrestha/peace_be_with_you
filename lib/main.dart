0f09b7accc26eec2e6955e2ab09c1beca221516e
BabishShrestha <babishshrestha8@gmail.com>
2021-05-18 19:48:47 +0545

    Added SingleChildScrollView to scroll Added a Layout Builder to be Adaptive

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,34 @@ class PeaceWithYou extends StatelessWidget {
         ),
         backgroundColor: Colors.lightBlue[100],
         //Container used to contain and give spacing to the content inside it
-        body: Container(
-          padding: EdgeInsets.all(10),
-          //used to add multiple widgets in  a portrait layout
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //used to add rounded rectangular border to the image to look smooth
-              ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image(
-                  //used to add the needed image whether it be asset, network or other
-                  image: NetworkImage(
-                      //used to add image from the internet, when there's a connection available
-                      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Friver-and-trees-2.jpg&f=1&nofb=1'),
+        body: LayoutBuilder(builder: (context, constraints) {
+          print(constraints.maxWidth / 8);
+          print(constraints.maxHeight / 4);
+
+          return SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //used to add rounded rectangular border to the image to look smooth
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image(
+                    //used to add the needed image whether it be asset, network or other
+                    image: NetworkImage(
+                        //used to add image from the internet, when there's a connection available
+                        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fjooinn.com%2Fimages%2Friver-and-trees-2.jpg&f=1&nofb=1'),
+                  ),
                 ),
-              ),
-              Text('Peace Be With You. You are Calm.')
-              //used to indicate the quotes and message of App
-            ],
-          ),
-        ),
+                Text('Peace Be With You. You are Calm.')
+                //used to indicate the quotes and message of App
+              ],
+            ),
+            //used to add multiple widgets in  a portrait layout
+            padding: EdgeInsets.symmetric(
+                horizontal: constraints.maxWidth / 18,
+                vertical: constraints.maxHeight / 4),
+          );
+        }),
       ),
     );
   }
